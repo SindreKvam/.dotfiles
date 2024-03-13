e0b8ceef5fb5ab38007f8b977c686dd1c5d582a4
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2024-03-13 22:00:57 +0100

    Added brew.sh to install useful brew packages

---
+++ brew.sh
@@ -0,0 +1,16 @@
+# Default brew packages to install
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+
+# Install more recent versions of some macOS tools.
+brew install vim
+brew install git
+
+# Install useful brew packages
+brew install nmap
+brew install wakeonlan
