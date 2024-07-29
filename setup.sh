14387cf09d532d23f29b9f0551733b0645dc7168
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2024-07-29 22:34:57 +0200

    Updated setup to check OS

--- setup.sh
+++ setup.sh
@@ -1,26 +1,40 @@
 
 # Run this script while you are in the dotfiles directory
 
-# Install oh-my-zsh
-if [ -d "$HOME/.oh-my-zsh" ]; then
-    echo "oh-my-zsh already installed"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        echo "Linux"
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+        echo "Mac OSX"
+
+        # Install oh-my-zsh
+        if [ -d "$HOME/.oh-my-zsh" ]; then
+            echo "oh-my-zsh already installed"
+        else
+            sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        fi
+
+
+        # Get the absolute path of the script
+        SCRIPT=$(readlink -f "$0")
+        # Absolute path this script is in, thus /home/user/bin
+        SCRIPTPATH=$(dirname "$SCRIPT")
+        echo $SCRIPTPATH
+
+        # Create symbolic links
+        ln -s $SCRIPTPATH/.zshrc ~/.zshrc
+        echo .zshrc symlink created
+
+        ln -s $SCRIPTPATH/.vimrc ~/.vimrc
+        echo .vimrc symlink created
+
+        ln -s $SCRIPTPATH/.gitconfig ~/.gitconfig
+        echo .gitconfig symlink created
+
+        ln -s $SCRIPTPATH/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+        echo vscode settings.json symlink created
+
 else
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        echo "Unknown OS"
 fi
-
-
-# Get the absolute path of the script
-SCRIPT=$(readlink -f "$0")
-# Absolute path this script is in, thus /home/user/bin
-SCRIPTPATH=$(dirname "$SCRIPT")
-echo $SCRIPTPATH
-
-# Create symbolic links
-ln -s $SCRIPTPATH/.zshrc ~/.zshrc
-echo .zshrc symlink created
-
-ln -s $SCRIPTPATH/.vimrc ~/.vimrc
-echo .vimrc symlink created
-
-ln -s $SCRIPTPATH/.gitconfig ~/.gitconfig
-echo .gitconfig symlink created
