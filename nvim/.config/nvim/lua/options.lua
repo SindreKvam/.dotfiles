e20f8cfb51d1be687fc44e1ef4f57bab3aba2dbe
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2025-06-22 00:01:24 +0200

    Added basic neovim config

---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,13 @@
+
+-- Basic settings
+vim.opt.number = true          -- Show line numbers
+vim.opt.relativenumber = true  -- Relative line numbers
+vim.opt.tabstop = 4           -- Number of spaces that a <Tab> counts for
+vim.opt.shiftwidth = 4        -- Number of spaces to use for each step of (auto)indent
+vim.opt.expandtab = true      -- Use spaces instead of tabs
+vim.opt.smartindent = true    -- Smart autoindenting on new lines
+vim.opt.wrap = false          -- Disable line wrapping
+vim.opt.hlsearch = true       -- Highlight search results
+
+vim.opt.colorcolumn = 80      -- Highlight column 80
+
