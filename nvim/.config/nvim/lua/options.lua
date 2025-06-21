fee21a42c7c140e0a991d4a16d0670beca35c5f5
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2025-06-22 00:50:10 +0200

    Updated neovim settings

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,7 +1,7 @@
 
 -- Basic settings
-vim.opt.number = true          -- Show line numbers
-vim.opt.relativenumber = true  -- Relative line numbers
+vim.opt.nu = true        -- Show line numbers
+vim.opt.relativenumber = true -- Relative line numbers
 vim.opt.tabstop = 4           -- Number of spaces that a <Tab> counts for
 vim.opt.shiftwidth = 4        -- Number of spaces to use for each step of (auto)indent
 vim.opt.expandtab = true      -- Use spaces instead of tabs
@@ -9,5 +9,5 @@ vim.opt.smartindent = true    -- Smart autoindenting on new lines
 vim.opt.wrap = false          -- Disable line wrapping
 vim.opt.hlsearch = true       -- Highlight search results
 
-vim.opt.colorcolumn = 80      -- Highlight column 80
+vim.opt.colorcolumn = "80"      -- Highlight column 80
 
