59dddb960819d37ede1f300c02a42f69f287ddc0
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2025-06-26 23:06:40 +0200

    Added Nvim-tree package

--- nvim/.config/nvim/lua/lazyplugins.lua
+++ nvim/.config/nvim/lua/lazyplugins.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     if vim.v.shell_error ~= 0 then
         vim.api.nvim_echo({
             { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
+            { out,                            "WarningMsg" },
             { "\nPress any key to exit..." },
         }, true, {})
         vim.fn.getchar()
@@ -18,7 +18,8 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 
     {
-        'nvim-telescope/telescope.nvim', branch = '0.1.x',
+        'nvim-telescope/telescope.nvim',
+        branch = '0.1.x',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
     {
@@ -67,5 +68,7 @@ require("lazy").setup({
         'stevearc/conform.nvim',
         opts = {},
     },
+    {
+        "nvim-tree/nvim-tree.lua",
+    },
 })
-
--- nvim/.config/nvim/lua/remap.lua
+++ nvim/.config/nvim/lua/remap.lua
@@ -1,3 +1,1 @@
 vim.g.mapleader = " "
-
-vim.keymap.set({"n"}, '<leader>ex', ":Ex", {desc = "Open explorer"})
