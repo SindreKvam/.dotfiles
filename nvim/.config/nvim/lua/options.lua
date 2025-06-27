5b067713d50b4b2d22365e3bfb27e61d859747d3
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2025-06-27 13:12:27 +0200

    Moved line to 100 characters, reformatted file

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,34 +1,31 @@
-
 -- Basic settings
-vim.opt.nu = true        -- Show line numbers
+vim.opt.nu = true -- Show line numbers
 vim.opt.relativenumber = true -- Relative line numbers
-vim.opt.tabstop = 4           -- Number of spaces that a <Tab> counts for
-vim.opt.shiftwidth = 4        -- Number of spaces to use for each step of (auto)indent
-vim.opt.expandtab = true      -- Use spaces instead of tabs
-vim.opt.smartindent = true    -- Smart autoindenting on new lines
-vim.opt.wrap = false          -- Disable line wrapping
-vim.opt.hlsearch = true       -- Highlight search results
+vim.opt.tabstop = 4 -- Number of spaces that a <Tab> counts for
+vim.opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.smartindent = true -- Smart autoindenting on new lines
+vim.opt.wrap = false -- Disable line wrapping
+vim.opt.hlsearch = true -- Highlight search results
 
 vim.opt.scrolloff = 8
 
-vim.opt.textwidth = 80
-vim.opt.colorcolumn = "80,100"   -- Highlight column
+vim.opt.textwidth = 100
+vim.opt.colorcolumn = "100" -- Highlight column
 
 -- Warning colors when length exceeds recommended length
-vim.cmd [[highlight OverLength ctermbg=darkred guibg=#592929]]
+vim.cmd([[highlight OverLength ctermbg=darkred guibg=#592929]])
 
 local function highlight_overlength()
-    local col = vim.opt.textwidth:get()
-    vim.fn.matchadd("Overlength", "\\%>" .. col .. "v.", 100)
+	local col = vim.opt.textwidth:get()
+	vim.fn.matchadd("Overlength", "\\%>" .. col .. "v.", 100)
 end
 
-vim.api.nvim_create_autocmd( {"InsertLeave","BufWinEnter"}, {
-    callback = highlight_overlength,
+vim.api.nvim_create_autocmd({ "InsertLeave", "BufWinEnter" }, {
+	callback = highlight_overlength,
 })
-
 
 -- Diagnostics configuration
 vim.diagnostic.config({
-    virtual_text = true,
+	virtual_text = true,
 })
-
