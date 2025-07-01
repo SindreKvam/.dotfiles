498d54298b1229094635156601204e513ee4c04b
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2025-07-01 14:54:08 +0200

    Added some more remaps

--- nvim/.config/nvim/lua/remap.lua
+++ nvim/.config/nvim/lua/remap.lua
@@ -1,1 +1,8 @@
 vim.g.mapleader = " "
+
+-- Copy to clipboard
+vim.keymap.set("n", "<leader>yy", '"+yy')
+vim.keymap.set("v", "<leader>y", '"+y')
+
+vim.keymap.set({ "n", "v", "o" }, "ø", "^")
+vim.keymap.set({ "n", "v", "o" }, "æ", "$")
