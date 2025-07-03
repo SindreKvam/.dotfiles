ef17b1786fb4e83fd2a468226d3b556931726818
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2025-07-03 10:25:45 +0200

    Updated nice-to-have shortcuts

--- nvim/.config/nvim/lua/remap.lua
+++ nvim/.config/nvim/lua/remap.lua
@@ -1,8 +1,15 @@
 vim.g.mapleader = " "
 
+-- Update half page jumps to center the cursor
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
 -- Copy to clipboard
 vim.keymap.set("n", "<leader>yy", '"+yy')
 vim.keymap.set("v", "<leader>y", '"+y')
 
 vim.keymap.set({ "n", "v", "o" }, "ø", "^")
 vim.keymap.set({ "n", "v", "o" }, "æ", "$")
+
+vim.keymap.set("n", "<leader>|", ":vsplit<Cr>")
+vim.keymap.set("n", "<leader>-", ":split<Cr>")
