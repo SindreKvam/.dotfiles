2d4c4da54cd63a4ecf5784859fa25c2f6245e394
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2025-07-29 01:52:35 +0200

    Fix paste from machine clipboard

--- nvim/.config/nvim/lua/remap.lua
+++ nvim/.config/nvim/lua/remap.lua
@@ -7,6 +7,7 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 -- Copy to clipboard
 vim.keymap.set("n", "<leader>yy", '"+yy')
 vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set({ "n", "v" }, "<leader>p", '"+p')
 
 vim.keymap.set({ "n", "v", "o" }, "ø", "^")
 vim.keymap.set({ "n", "v", "o" }, "æ", "$")
