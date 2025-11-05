4f489aa85ec64be7450e93c6847769af3c792fee
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2025-11-05 19:54:20 +0100

    Transparent background for nvim

--- nvim/.config/nvim/after/plugin/onedark.lua
+++ nvim/.config/nvim/after/plugin/onedark.lua
@@ -115,10 +115,10 @@ require("onedarkpro").setup({
     },
 
     options = {
-        cursorline = false,                 -- Use cursorline highlighting?
-        transparency = false,               -- Use a transparent background?
+        cursorline = true,                 -- Use cursorline highlighting?
+        transparency = true,               -- Use a transparent background?
         terminal_colors = true,             -- Use the theme's colors for Neovim's :terminal?
-        lualine_transparency = false,       -- Center bar transparency?
+        lualine_transparency = true,       -- Center bar transparency?
         highlight_inactive_windows = false, -- When the window is out of focus, change the normal background?
     }
 })
