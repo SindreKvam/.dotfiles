a3956b2c1b51c86f4921b909c488482e333f2096
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2025-11-07 12:53:55 +0100

    Updated conform config, formatted lsp config and removed python specific settings

--- nvim/.config/nvim/after/plugin/conform.lua
+++ nvim/.config/nvim/after/plugin/conform.lua
@@ -1,15 +1,23 @@
 local conform = require("conform")
 conform.setup({
-    formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "ruff" },
-    },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = {
+			"ruff",
+			-- To fix auto-fixable lint errors.
+			"ruff_fix",
+			-- To run the Ruff formatter.
+			"ruff_format",
+			-- To organize the imports.
+			"ruff_organize_imports",
+		},
+	},
 })
 
 vim.keymap.set("n", "<S-A-F>", function()
-    conform.format({ lsp_fallback = true })
+	conform.format({ lsp_fallback = true })
 end, { desc = "Format document" })
 
 vim.keymap.set("v", "<S-A-F>", function()
-    conform.format({ lsp_fallback = true })
+	conform.format({ lsp_fallback = true })
 end, { desc = "Format selection" })
--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -4,147 +4,125 @@ local lspkind = require("lspkind")
 local luasnip = require("luasnip")
 
 mason_lspconfig.setup({
-    ensure_installed = {
-        "pylsp", -- python LSP server
-        "ruff",  -- Linter and formatter for python
-        "lua_ls",
-        "vtsls",
-    },
-    handlers = {
-        function(server_name)
-            require("lspconfig")[server_name].setup({})
-        end,
-    },
-    exclude = {
-        "clangd",
-        "rust_analyzer",
-    },
+	ensure_installed = {
+		"ruff", -- Linter and formatter for python
+		"stylua", -- Lua formatter
+		"lua_ls",
+		"vtsls",
+	},
+	handlers = {
+		function(server_name)
+			require("lspconfig")[server_name].setup({})
+		end,
+	},
+	exclude = {
+		"clangd",
+		"rust_analyzer",
+	},
 })
 
 if vim.fn.executable("clangd") then
-    vim.lsp.enable("clangd")
+	vim.lsp.enable("clangd")
 end
 if vim.fn.executable("rust_analyzer") then
-    vim.lsp.enable("rust_analyzer")
+	vim.lsp.enable("rust_analyzer")
 end
 
 -- Remove warning for undefined vim
 vim.lsp.config("lua_ls", {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { "vim" },
-            },
-        },
-    },
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
 })
-
--- Python-LSP config
--- This config is only for linting
--- See conform.lua for formatting
-vim.lsp.config("pylsp", {
-    settings = {
-        pylsp = {
-            plugins = {
-                pylint = { enabled = false },
-                ruff = { enabled = true },
-                black = { enabled = false },
-                flake8 = { enabled = false },
-                pycodestyle = { enabled = false },
-                pyflakes = { enabled = false },
-                mccabe = { enabled = false },
-            },
-        },
-    },
-})
-
 
 -- luasnip
 -- Add friendly snippets
 require("luasnip.loaders.from_vscode").lazy_load()
 -- Add custom snippets
 require("luasnip.loaders.from_vscode").lazy_load({
-    paths = { "~/.config/nvim/snippets" }
+	paths = { "~/.config/nvim/snippets" },
 })
 
 -- setup cmp
 cmp.setup({
-    sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "nvim-lsp-signature-help" },
-        -- { name = "vsnip" }, -- For vsnip users.
-        { name = "luasnip" }, -- "saadparwaiz1/cmp_luasnip"
-        { name = "buffer" },
-    }),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "nvim-lsp-signature-help" },
+		{ name = "luasnip" }, -- "saadparwaiz1/cmp_luasnip"
+		{ name = "buffer" },
+	}),
 
-    snippet = {
-        expand = function(args)
-            require("luasnip").lsp_expand(args.body) -- for `luasnip` users.
-        end,
-    },
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body) -- for `luasnip` users.
+		end,
+	},
 
-    formatting = {
-        fields = { "abbr", "kind", "menu" },
-        expandable_indicator = true,
-        format = lspkind.cmp_format({
-            -- mode = 'symbol', -- show only symbol annotations
-            -- maxwidth = 50,
-            -- ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-            show_labeldetails = true, -- show labeldetails in menu. disabled by default
-            before = function(entry, vim_item)
-                return vim_item
-            end,
-        }),
-    },
+	formatting = {
+		fields = { "abbr", "kind", "menu" },
+		expandable_indicator = true,
+		format = lspkind.cmp_format({
+			-- mode = 'symbol', -- show only symbol annotations
+			-- maxwidth = 50,
+			-- ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+			show_labeldetails = true, -- show labeldetails in menu. disabled by default
+			before = function(entry, vim_item)
+				return vim_item
+			end,
+		}),
+	},
 
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-    },
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
 
-    mapping = cmp.mapping.preset.insert({
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
 
-        ['<CR>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                if luasnip.expandable() then
-                    luasnip.expand()
-                else
-                    cmp.confirm({
-                        select = true,
-                    })
-                end
-            else
-                fallback()
-            end
-        end),
+		["<CR>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				if luasnip.expandable() then
+					luasnip.expand()
+				else
+					cmp.confirm({
+						select = true,
+					})
+				end
+			else
+				fallback()
+			end
+		end),
 
-        -- Luasnippet, press tab to jump to next input field
-        -- or Shift-tab to jump back to previous
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.locally_jumpable(1) then
-                luasnip.jump(1)
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
+		-- Luasnippet, press tab to jump to next input field
+		-- or Shift-tab to jump back to previous
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.locally_jumpable(1) then
+				luasnip.jump(1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
 
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.locally_jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-
-    }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.locally_jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+	}),
 })
 
 -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
@@ -161,19 +139,19 @@ require("cmp_git").setup() ]]
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline({ "/", "?" }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-        { name = "buffer" },
-    },
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	},
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(":", {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-        { name = "path" },
-    }, {
-        { name = "cmdline" },
-    }),
-    matching = { disallow_symbol_nonprefix_matching = false },
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
+	matching = { disallow_symbol_nonprefix_matching = false },
 })
